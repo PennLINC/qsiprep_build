135284252a975ae33483b5623615956e0854644b
Matt Cieslak <mattcieslak@gmail.com>
2022-10-19 11:03:42 -0400

    update tortoise tag

--- setup_build.sh
+++ setup_build.sh
@@ -16,7 +16,7 @@ export TAG_3TISSUE=22.1.0
 export TAG_DSISTUDIO=22.2.10
 export TAG_MINICONDA=22.4.1
 export TAG_AFNI=22.2.0
-export TAG_TORTOISE=22.9.0
+export TAG_TORTOISE=22.10.0
 
 echo "Settings:"
 echo "----------"
