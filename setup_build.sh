51036dd3fd9e08f77311e6b08fa4fbb28ba363de
Matt Cieslak <mattcieslak@gmail.com>
2022-10-25 17:36:06 -0400

    Add back 3dZeropad and 3dSeg

--- setup_build.sh
+++ setup_build.sh
@@ -15,7 +15,7 @@ export TAG_MRTRIX3=22.1.0
 export TAG_3TISSUE=22.1.0
 export TAG_DSISTUDIO=22.2.10
 export TAG_MINICONDA=22.10.3
-export TAG_AFNI=22.2.0
+export TAG_AFNI=22.10.5
 export TAG_TORTOISE=22.10.1
 
 echo "Settings:"
