3f786b965521568d04591663dd1dfe6c675a524c
Matt Cieslak <mattcieslak@gmail.com>
2023-02-17 17:14:27 -0500

    New ANTs with ITK 5.3

--- setup_build.sh
+++ setup_build.sh
@@ -10,13 +10,13 @@ export BUILD_TAG
 # Versions of the components
 export TAG_FSL=22.9.0
 export TAG_FREESURFER=22.10.3
-export TAG_ANTS=22.10.1
+export TAG_ANTS=23.2.0
 export TAG_MRTRIX3=22.1.0
 export TAG_3TISSUE=22.1.0
 export TAG_DSISTUDIO=22.2.10
 export TAG_MINICONDA=22.10.3
 export TAG_AFNI=22.10.5
-export TAG_TORTOISE=22.10.1
+export TAG_TORTOISE=23.2.0
 
 echo "Settings:"
 echo "----------"
