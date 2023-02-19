1ce24a308ab06b56e5cae307643fe7c70dcc2e50
Matt Cieslak <mattcieslak@gmail.com>
2023-02-18 21:35:59 -0500

    Add AFNI build

--- setup_build.sh
+++ setup_build.sh
@@ -15,8 +15,8 @@ export TAG_MRTRIX3=22.1.0
 export TAG_3TISSUE=22.1.0
 export TAG_DSISTUDIO=22.2.10
 export TAG_MINICONDA=22.10.3
-export TAG_AFNI=23.2.1
-export TAG_TORTOISE=23.2.1
+export TAG_AFNI=23.2.2
+export TAG_TORTOISE=23.2.2
 
 echo "Settings:"
 echo "----------"
